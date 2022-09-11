8db2ad874b706738ae552ea0353c58bd2a401938
Sechaba Machaba <machabasechaba@gmail.com>
2022-09-11 12:28:29 -0700

    hi

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i
+	float f
+	char c
+
+	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(c));
+	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of long int:%lu byte(s)\n",(unsigned long)sizeof(i));
+	printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(i));
+	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(f));
+	return (0);
+}
