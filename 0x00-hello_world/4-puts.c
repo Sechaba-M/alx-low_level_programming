0fd2c03e9ffc448d527429168d9e80273bec5022
Sechaba Machaba <machabasechaba@gmail.com>
2022-09-11 10:08:38 -0700

    hi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 /**
- * main willbe the entrie point for 
+ * main - entry point
+ *
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
+	
 	puts(str);
 	return (0);
 }
