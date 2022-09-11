cd2d31043d50f7d2b7204b4aa43fcf57b543ee8b
Sechaba Machaba <machabasechaba@gmail.com>
2022-09-11 10:02:11 -0700

    hi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,11 @@
 
 /**
  * main willbe the entrie point for 
- * the c program
- * Returns the the string below 
- * */
+ */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
-	
-	
+
 	puts(str);
 	return (0);
 }
