13576677233cda53d7afc338ce79ef8f8c70561c
Sechaba Machaba <machabasechaba@gmail.com>
2022-09-11 12:36:11 -0700

    hi

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,12 @@ int main(void)
 	int i;
 	float f;
 	char c;
+	long int li;
 
 	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(c));
 	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(i));
 	printf("Size of long int:%lu byte(s)\n",(unsigned long)sizeof(i));
-	printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(i));
+	printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(li));
 	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(f));
 	return (0);
 }
