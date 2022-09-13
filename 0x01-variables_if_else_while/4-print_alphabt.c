c195ffc7c49135d5d7a1e474d9808ec9c9892805
Sechaba Machaba <machabasechaba@gmail.com>
2022-09-12 20:37:56 -0700

    hi

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: using the main function
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -enty point
+ *
+ * Return; Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
