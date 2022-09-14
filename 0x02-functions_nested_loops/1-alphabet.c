acb80215a2342aa479c4a006db661b7f98c9b730
Sechaba Machaba <machabasechaba@gmail.com>
2022-09-14 06:21:39 -0700

    hi

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,15 @@
 #include "main.h"
 /**
  * main - entry point
+ * vod print-alphabet(void)
+ *
  *
  * return : 0 (Success)
  */
 void print_alphabet(void)
 {
 	char c;
+
 	for(c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
