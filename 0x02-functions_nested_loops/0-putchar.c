c8ed10f6e279855fb7220e62149f687071d333c1
Sechaba Machaba <machabasechaba@gmail.com>
2022-09-14 02:27:53 -0700

    hi

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,16 +2,17 @@
 #include <main.h>
 #include <_putchar.c>
 /**
- * main - entry point
+ * main - entry poinit
  *
  * Return: 0 (successful)
  */
+
+int _putchar(char c)
+
 int main(void)
 {
 	char message = "_putchar\n";
-	
-	putchar(message);
-
+	_putchar(message);
 	return (0);
 }
 
