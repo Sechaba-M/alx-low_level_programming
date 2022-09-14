79a6fccc6ea8a5b9dca8fe634d6030dfa2b62ac6
Sechaba Machaba <machabasechaba@gmail.com>
2022-09-14 05:35:52 -0700

    hi

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * return : 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+	for(c='a' ; c<=z; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0)
+}
