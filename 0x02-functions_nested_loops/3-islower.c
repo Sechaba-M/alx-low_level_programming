f01df5e6a5b00fe9f3f2385b4c9fafb6f595363c
Sechaba Machaba <machabasechaba@gmail.com>
2022-09-14 07:51:46 -0700

    hi

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main -entry point
+ *
+ * _islower - check if letter in lower case
+ *
+ * Retuns: 1 if lower cas and 0 otherwise 
+ */
+
+int _islower(int c)
+{
+	int ret;
+	if (c >= 123 ||  c <= 96)
+	{
+		ret = 0
+	}
+	else
+	{
+		ret = 1
+	}
+	return ret
+}
+
