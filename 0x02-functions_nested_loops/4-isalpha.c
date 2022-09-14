48d091e0b81a0eed5aa022532eeb51a801ec1588
Sechaba Machaba <machabasechaba@gmail.com>
2022-09-14 08:27:03 -0700

    hi

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _isalpha - check if the character is a aplphabet upper case and lower case 
+ *
+ * c - ascii intiger value of the character
+ *
+ * Return: 1 or 0 depending on the bollean
+ */
+int _isalpha(int c)
+{
+	int ret;
+
+	if ( c < 64 || c > 122 )
+	{
+		ret = 0;
+	}
+	else if (c >= 91 && c <= 96)
+	{ 
+		ret = 0;
+	}
+	else 
+	{
+		ret = 1;
+	}
+	return ret
+}
