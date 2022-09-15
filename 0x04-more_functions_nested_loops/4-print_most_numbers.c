e358ebe810e006b2ad2ae4e0a4b4908f4418d83b
Sechaba Machaba <machabasechaba@gmail.com>
2022-09-15 07:53:01 -0700

    hi

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		if (a == 2 || a == 4) 
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(a)
+		}
+	}
+	_putchar('\n');
+}
